b4f1b410e3afccc6e06de20e49845e50bd308095
gaetan <gaetansdx@gmail.com>
2018-09-11 22:36:22 +0200

    Ajout semantic / création ajout de post + affichage début

---
+++ include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <title>Sadous Ga&euml;tan Bts Sio Option Slam</title>
+
+    <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script
+            src="jquery/jquery-3.3.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous">
+    </script>
+    <script src="semantic/semantic.min.js"></script>
+
+</head>
+
+<a href="../"><div class="textEntete">Site vers la page de projets</div></a>
+
+<hr>
+
